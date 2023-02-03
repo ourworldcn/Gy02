9cf20c3143f5c68998f0c7df38adf0ed0d1c920f
ourworld_cn <ourworld_cn@msn.com>
2023-02-03 19:06:05 +0800

    日提交

--- OwBaseCore/Server/OwScheduler.cs
+++ OwBaseCore/Server/OwScheduler.cs
@@ -6,12 +6,14 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace OW.OW.Server
+namespace OW.Server
 {
     public class OwSchedulerEntry
     {
@@ -41,6 +43,9 @@ namespace OW.OW.Server
         public DateTime LastUtc { get; internal set; }
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
     public class OwSchedulerOptions : IOptions<OwSchedulerOptions>
     {
         /// <summary>
@@ -111,46 +116,117 @@ namespace OW.OW.Server
                 using var dw = DisposeHelper.Create(Options.LockCallback, Options.UnlockCallback, kvp.Key, TimeSpan.Zero);
                 if (dw.IsEmpty) //若无法锁定
                 {
-                    Plan(kvp.Key);
+                    Run(kvp.Key, false);
                     continue;
                 }
                 if (kvp.Value.Period == Timeout.InfiniteTimeSpan)   //若是无限周期的
                     continue;
-                if (DateTime.UtcNow - kvp.Value.LastUtc > kvp.Value.Period)
+                var now = DateTime.UtcNow;
+                if (now - kvp.Value.LastUtc >= kvp.Value.Period)    //若周期时间已到
                 {
                     RunCore(kvp.Key);
-                    kvp.Value.LastUtc = DateTime.UtcNow;
                 }
             }
         }
 
         /// <summary>
+        /// 优先执行的任务的任务对象。
+        /// </summary>
+        Task _Task;
+
+        /// <summary>
         /// 将优先计划的项全部执行。
         /// </summary>
-        private void RunPlan()
+        private void RunPlanFunc()
         {
-            DisposeHelper<object[]> dw;
-            int count;
-            Span<object> span;
+            List<object> keys;
             lock (_Plans)
             {
-                count = _Plans.Count;
-                var ary = ArrayPool<object>.Shared.Rent(count);
-                dw = DisposeHelper.Create(c => ArrayPool<object>.Shared.Return(c), ary);
-                span = ary.AsSpan(0, count);
+                keys = new List<object>(_Plans);
+                _Plans.Clear();
             }
-            using var s = dw;
-
+            foreach (var key in keys)
+            {
+                using var dw = DisposeHelper.Create(Options.LockCallback, Options.UnlockCallback, key, TimeSpan.Zero);
+                if (dw.IsEmpty || !RunCore(key)) //若无法锁定或执行不成功
+                {
+                    lock (_Plans)
+                        _Plans.Add(key);
+                    continue;
+                }
+            }
         }
 
+        /// <summary>
+        /// 配置对象。
+        /// </summary>
         public OwSchedulerOptions Options { get; init; }
 
+        /// <summary>
+        /// 任务项。
+        /// </summary>
         ConcurrentDictionary<object, OwSchedulerEntry> _Items = new ConcurrentDictionary<object, OwSchedulerEntry>();
 
+        /// <summary>
+        /// 优先执行任务的列表。
+        /// 锁定此对象时，不可再试图锁定任何key。
+        /// </summary>
+        HashSet<object> _Plans = new HashSet<object>();
+
+        /// <summary>
+        /// 执行指定键值的任务。
+        /// 非公有函数不会自动对键加锁，若需要则调用者需负责加/解锁。
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns>同步执行时返回任务的返回值。异步执行时返回true。</returns>
         protected virtual bool RunCore(object key)
         {
             var entry = _Items.GetValueOrDefault(key);
-            return entry.TaskCallback?.Invoke(key, entry.State) ?? true;
+            var b = entry.TaskCallback?.Invoke(key, entry.State) ?? true;
+            if (b)
+                entry.LastUtc = DateTime.UtcNow;
+            return b;
+        }
+
+        /// <summary>
+        /// 执行指定键值的任务。
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="sync">true同步执行，false(默认)计划优先执行。</param>
+        /// <returns>同步执行时返回任务的返回值。异步执行时返回true标识成功增加优先任务，false表示优先队列中有一个同样的项尚未执行。</returns>
+        public bool Run(object key, bool sync = false)
+        {
+            if (sync) //若同步执行
+            {
+                using var dw = DisposeHelper.Create(Options.LockCallback, Options.UnlockCallback, key, Timeout.InfiniteTimeSpan);
+                var b = RunCore(key);
+                if (b)
+                    lock (_Plans)
+                        _Plans.Remove(key);
+                return b;
+            }
+            else //若计划优先执行
+            {
+                lock (_Plans)
+                {
+                    var b = _Plans.Add(key);
+                    if (b)
+                        SchedulerTaskCore();
+                    return b;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 计划执行优先任务。不可重入，不能多线程并发。
+        /// </summary>
+        private void SchedulerTaskCore()
+        {
+            if (_Task is null || _Task.Wait(0)) //若没有任务或已经完成任务
+            {
+                _Task?.Dispose();
+                _Task = Task.Run(RunPlanFunc);
+            }
         }
 
         /// <summary>
@@ -172,34 +248,15 @@ namespace OW.OW.Server
         /// <returns></returns>
         public bool TryRemove(object key)
         {
-            using (var dw = DisposeHelper.Create(Options.LockCallback, Options.UnlockCallback, key, Timeout.InfiniteTimeSpan))
-                return _Items.Remove(key, out _);
-        }
-
-        /// <summary>
-        /// 在该函数内立即执行指定的任务。
-        /// </summary>
-        public bool EnsureRun(object key)
-        {
             using var dw = DisposeHelper.Create(Options.LockCallback, Options.UnlockCallback, key, Timeout.InfiniteTimeSpan);
-            if (dw.IsEmpty)
-                return false;
-            return RunCore(key);
+            bool result = _Items.Remove(key, out _);
+            if (result)  //若成功移除任务
+                lock (_Plans)
+                {
+                    _Plans.Remove(key);
+                }
+            return result;
         }
 
-        /// <summary>
-        /// 优先执行任务的列表。
-        /// </summary>
-        HashSet<object> _Plans = new HashSet<object>();
-
-        /// <summary>
-        /// 无视定时优先执行安排指定任务。
-        /// 不会立即执行而是在下次扫描时执行。
-        /// </summary>
-        public void Plan(object key)
-        {
-            lock (_Plans)
-                _Plans.Add(key);
-        }
     }
 }
