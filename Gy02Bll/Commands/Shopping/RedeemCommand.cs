c53263ea7eca937922a354a0ecb512f15f6318eb
ourworld_cn <ourworld_cn@msn.com>
2023-11-21 19:18:10 +0800

    初步完成兑换码所有功能。

--- Gy02Bll/Commands/Shopping/RedeemCommand.cs
+++ Gy02Bll/Commands/Shopping/RedeemCommand.cs
@@ -1,6 +1,9 @@
 ﻿using GY02.Managers;
+using GY02.Publisher;
+using Microsoft.EntityFrameworkCore;
 using OW.Game;
 using OW.Game.Entity;
+using OW.Game.Store;
 using OW.SyncCommand;
 using System;
 using System.Collections.Generic;
@@ -20,18 +23,47 @@ namespace GY02.Commands
         public string Code { get; set; }
     }
 
+    /// <summary>
+    /// 使用兑换码兑换。
+    /// </summary>
     public class RedeemCommandHandler : SyncCommandHandlerBase<RedeemCommand>, IGameCharHandler<RedeemCommand>
     {
-        public RedeemCommandHandler(GameAccountStoreManager accountStore)
+        public RedeemCommandHandler(GameAccountStoreManager accountStore, GY02UserContext dbContext, SyncCommandManager syncCommand)
         {
             AccountStore = accountStore;
+            _DbContext = dbContext;
+            _syncCommand = syncCommand;
         }
 
         public GameAccountStoreManager AccountStore { get; }
+        GY02UserContext _DbContext;
+        SyncCommandManager _syncCommand;
 
         public override void Handle(RedeemCommand command)
         {
-
+            var redeem = _DbContext.GameRedeemCodes.Find(command.Code);
+            if (redeem is null)  //若指定兑换码无效
+            {
+                command.ErrorCode = ErrorCodes.ERROR_BAD_ARGUMENTS;
+                return;
+            }
+            var catalog = _DbContext.GameRedeemCodeCatalogs.FirstOrDefault(c => c.Id == redeem.CatalogId);
+            if (catalog is null)
+            {
+                command.ErrorCode = ErrorCodes.ERROR_BAD_ARGUMENTS;
+                return;
+            }
+            var tid = catalog.ShoppingTId;
+            var shopping = new ShoppingBuyCommand
+            {
+                GameChar = command.GameChar,
+                ShoppingItemTId = tid,
+                Count = 1,
+                Changes = command.Changes,
+            };
+            _syncCommand.Handle(shopping);
+            if (shopping.HasError)
+                command.FillErrorFrom(shopping);
         }
     }
 }
